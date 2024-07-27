81ea3bae2ec1309d83a97dadd23fa89b6a6b1acd
mannyamm21 <130400515+mannyamm21@users.noreply.github.com>
2024-07-27 14:59:18 +0530

    new

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -55,15 +55,16 @@ const projectsData = [
     gitUrl: "https://github.com/mannyamm21/ReactPortfolio",
     previewUrl: "https://github.com/mannyamm21/ReactPortfolio",
   },
-  // {
-  //   id: 6,
-  //   title: "Full-stack Roadmap",
-  //   description: "Project 5 description",
-  //   image: "/images/projects/6.png",
-  //   tag: ["All", "Web"],
-  //   gitUrl: "/",
-  //   previewUrl: "/",
-  // },
+  {
+    id: 6,
+    title: "VideoFusion",
+    description:
+      "Developed VideoFusion using React and Node.js, optimizing performance and implementing a recommendation algorithm. Utilized Firebase for storage, added email functionality, and integrated Cloudinary for images, boosting engagement and retention.",
+    image: "/images/projects/VideoFusion.png",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
 ];
 
 const ProjectsSection = () => {
